c3bb7d605522e029a9e81ed62a65ec5bea8cae22
Jonathan Gomez <jonakmex@gmail.com>
2026-04-02 16:30:57 -0600

    feat: Initial command querybalancecommand

--- src/main/java/com/clubix/repository/entity/CustomerEntity.java
+++ src/main/java/com/clubix/repository/entity/CustomerEntity.java
@@ -2,10 +2,14 @@ package com.clubix.repository.entity;
 
 import lombok.Builder;
 import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
 
 @Data
 @Builder
+@Table("customer")
 public class CustomerEntity {
+    @Id
     private String id;
     private Double balance;
 }
